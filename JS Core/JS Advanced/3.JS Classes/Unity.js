d38aee7cc516ed62d6dbda541a836e3160c765e2
Radoslav Tsvetkov <geomilevinc@gmail.com>
2019-06-06 15:16:45 +0300

    Add files via upload

---
+++ JS Core/JS Advanced/3.JS Classes/Unity.js
@@ -0,0 +1,33 @@
+class Rat {
+    constructor(name) {
+        this.name = name;
+        this.unitedRats = [];
+    }
+
+    unite(otherRat) {
+        if (otherRat instanceof Rat) {
+            this.unitedRats.push(otherRat);
+        } 
+    }
+
+    getRats() {
+        return this.unitedRats;
+    }
+
+    toString() {
+        let output = `${this.name}\n`;
+
+        for (let rat of this.unitedRats) {
+            output += `##${rat.name}\n`;
+        }
+
+        return output;
+    }
+}
+
+let rat = new Rat('Pesho');
+let rat2 = new Rat('Gosho');
+let rat3 = new Rat('Stamat');
+rat.unite(rat2);
+rat.unite(rat3);
+console.log(rat.toString());
