ffb0f6f48b01027022f8e28dd45e32bb240f7984
Rio <geomilevinc@gmail.com>
2019-12-17 12:26:12 +0200

    Added author model

--- React/rest/config/routes.js
+++ React/rest/config/routes.js
@@ -17,5 +17,7 @@ module.exports = (app) => {
 
     app.use('/api/book', router.book);
 
-    app.use('*', (req, res, next) => res.send('<h1> Something went wrong. Try again. </h1>'))
+    app.use('/api/author', router.author);
+
+    app.use('*', (req, res, next) => res.send('<h1> Something went wrong. </h1>'))
 };
