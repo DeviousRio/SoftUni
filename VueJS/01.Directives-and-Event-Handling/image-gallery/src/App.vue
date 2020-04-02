97e32a712dd2da8be95bfdf2d87dc16196e5385b
Rio <geomilevinc@gmail.com>
2020-04-02 11:57:14 +0300

    Added Products - problem solved

--- VueJS/01.Directives-and-Event-Handling/image-gallery/src/App.vue
+++ VueJS/01.Directives-and-Event-Handling/image-gallery/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <HelloWorld />
+    <Products />
     <Gallery />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Products from './components/Products.vue';
 import Gallery from './components/Gallery.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    Products,
     Gallery
   }
 }
---
+++ VueJS/01.Directives-and-Event-Handling/image-gallery/src/components/Products.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Fruits and Veggies</h1>
+    <ul v-for="product in products" :key="product.id">
+      <li :class="product.type == 'fruit' ? 'fruit' : 'vegetable'">
+        <span>{{product.name}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products",
+  data: function() {
+    return {
+      products: [
+        { id: 1, type: "fruit", name: "Grape" },
+        { id: 2, type: "vegetable", name: "Cucumber" },
+        { id: 3, type: "fruit", name: "Banana" },
+        { id: 4, type: "vegetable", name: "Carrot" },
+        { id: 5, type: "fruit", name: "Apple" },
+        { id: 6, type: "vegetable", name: "Potato" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  padding: 0;
+}
+
+ul li {
+  width: 100px;
+  list-style-type: none;
+  margin: 16px auto;
+  padding: 2px;
+}
+
+.fruit {
+  background-color: skyblue;
+  border: 2px solid red;
+}
+
+.vegetable {
+  background-color: orange;
+  border: 2px solid black;
+}
+</style>
