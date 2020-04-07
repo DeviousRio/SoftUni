b4d4d1a78c52135d0b3d38ad7345eef182e0c4cf
Rio <geomilevinc@gmail.com>
2020-04-07 12:50:22 +0300

    Initial commit

--- VueJS/01.Directives-and-Event-Handling/image-gallery/src/components/Gallery.vue
+++ VueJS/01.Directives-and-Event-Handling/image-gallery/src/components/Gallery.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
     <h2>Image Gallery</h2>
+
+    <form @submit.prevent="addImage('Message', $event)">
+      <label for="img-source">Source:</label>
+      <input :value="imgSrc" @input="setImgSource" type="text" id="img-source" placeholder="Your image source...">
+
+      <label for="img-alt">Alt:</label>
+      <input :value="imgAlt" @input="setImgAlt" type="text" id="img-alt" placeholder="Your image alt...">
+
+      <label for="img-desc">Description:</label>
+      <textarea :value="imgDescription" @input="setImgDescription" id="img-desc" placeholder="Your image description..."></textarea>
+
+      <input class="btn-add" type="submit" value="Submit">
+    </form>
+
     <div class="gallery" v-for="img in gallery" :key="img.id">
       <a>
         <img :src="img.imgSrc" :alt="img.imgAlt" />
@@ -62,13 +76,43 @@ export default {
           description: "Mountains"
         }
       ],
-      message: "My custom gallery!"
+      message: "My custom gallery!",
+      imgSrc: "",
+      imgAlt: "",
+      imgDescription: ""
     };
   },
   methods: {
     toggleDescription(img) {
       this.message = "Change message description";
       img.showDescription = !img.showDescription;
+    },
+    setImgSource(e) {
+      const value = e.target.value;
+      this.imgSrc = value;
+    },
+    setImgAlt(e) {
+      const value = e.target.value;
+      this.imgAlt = value;
+    },
+    setImgDescription(e) {
+      const value = e.target.value;
+      this.imgDescription = value;
+    },
+    addImage(msg, e) {
+      console.log(msg);
+      console.dir(e.target);
+
+      this.gallery.push({
+        imgSrc: this.imgSrc,
+        imgAlt: this.imgAlt,
+        description: this.imgDescription,
+        showDescription: false
+      });
+
+      this.imgSrc = "";
+      this.imgAlt = "";
+      this.imgDescription = "";
     }
   }
 };
@@ -81,6 +125,22 @@ export default {
   max-width: 960px;
   padding-right: 10px; /* 3 */
   padding-left: 10px; /* 3 */
+}
+
+form {
+  margin-bottom: 20px;
+}
+
+input[type="text"],
+textarea {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+
+textarea {
+  resize: none;
 }
 
 .gallery {
@@ -100,14 +160,28 @@ export default {
   cursor: pointer;
 }
 
+button {
+  margin: 10px;
+}
+
 .descripion {
   padding: 15px;
   text-align: center;
   font-size: 20px;
 }
 
-.show-desc,
 .btn-add {
+  cursor: pointer;
+  background-color: #ba0000;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+.show-desc {
   cursor: pointer;
   background-color: #008cba;
   border: none;
---
+++ VueJS/02.Components/components-demo/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="counter++">Counter: {{counter}}</button>    
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data: function() {
+        return {
+            counter: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        cursor: pointer;
+    }
+</style>
