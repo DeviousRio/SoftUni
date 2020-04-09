06862de3d744179b66e4bf8152d7d8b2ac5d94d5
Rio <geomilevinc@gmail.com>
2020-04-09 12:54:17 +0300

    Modified ProductList Component

--- VueJS/02.Components/components-demo/src/components/ProductList.vue
+++ VueJS/02.Components/components-demo/src/components/ProductList.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
+        {{title}}
+        <input type="text" :value="title" @input="changeTitle" />
+
         <h3>This is my product list:</h3>
-        <Product v-for="(p, i) in products" :key="i"
-            :type="p.type"
-            :price="p.price"
-            :ingredients="p.ingredients"        
-         />
+
+        <Product v-for="(p, i) in products" :key="i">
+            <h2 slot="type">Type: {{p.type}}</h2>
+            <h3 slot="price">Price: {{p.price}}</h3>
+            <ul slot="ingredients">
+                <li v-for="(ing, i2) in p.ingredients" :key="i2">{{ing}}</li>
+            </ul>
+        </Product>
     </div>
 </template>
 
@@ -15,7 +21,24 @@ import Product from './Product';
 export default {
     name: 'ProductList',
     props: {
-        products: Array
+        type: Array,
+        validator(arr) {
+            // Check all properties
+            return arr.every(p => typeof p.type === 'string'
+            && typeof p.price === 'number'
+            && Array.isArray(p.ingredients))
+        }
+    },
+    data: function() {
+        return {
+
+        }
+    },
+    methods: {
+        changeTitle(e) {
+            const value = e.target.value;
+            this.$emit('onChangeTitle', value);
+        }
     },
     components: {
         Product
