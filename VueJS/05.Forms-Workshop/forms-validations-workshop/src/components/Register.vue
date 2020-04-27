c2f2290ba34e503bfe689d747c85c616fee5ea93
DeviousRio <geomilevinc@gmail.com>
2020-04-27 13:30:20 +0300

    Initial commit

---
+++ VueJS/05.Forms-Workshop/forms-validations-workshop/src/components/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Register Form</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRegister'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
